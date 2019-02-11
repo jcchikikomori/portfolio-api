acbfccbacd6e924d9621914fd0ad0ee23cf2e9af
johncyrill <johncyril@gorated.ph>
2019-02-11 14:45:38 +0800

    Timestamps

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -8,6 +8,10 @@ module Types
 
     # https://graphql-ruby.org/api-doc/1.8.13/GraphQL/Types/Boolean
     field :deleted, GraphQL::BOOLEAN_TYPE, null: false
-    
+
+    # TODO: Format Timestamp either from here or from mutations
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :modified_at, GraphQL::Types::ISO8601DateTime, null: false
+
   end
 end
